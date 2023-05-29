43944540551e1e7b6710e0b131b684ffb0a75891
Digambaran <arjunsreekumar.1@gmail.com>
2023-05-29 15:53:17 +0530

    feat: division method added

--- math.js
+++ math.js
@@ -8,7 +8,11 @@ function subtract(a,b){
 }
 
 function multiply(a,b) {
-  return a*b
+  return a*b;
 }
 
-module.exports = {add, subtract, multiply}
+function divide(a,b) {
+  return a/b;
+}
+
+module.exports = {add, subtract, multiply, divide}
