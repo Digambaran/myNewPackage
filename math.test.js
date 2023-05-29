cecbad12ba91aa7df889208ae62ae29c3921b87c
Arjun S Kumar <arjunsreekumar.1@gmail.com>
2023-05-29 16:50:57 +0530

    Feature/multiply (#3)
    * chore: release-please yaml
    
    * feat: division method added
    
    * chore(workflow): fix release please

--- math.test.js
+++ math.test.js
@@ -1,4 +1,4 @@
-const {add, subtract, multiply} = require('./math.js')
+const {add, subtract, multiply, divide} = require('./math.js')
 
 test('Must add 2 and 3 to 5',()=>{
   expect(add(2,3)).toBe(5)
@@ -10,4 +10,8 @@ test('Must subtract 3 from 5 to 2',()=>{
 
 test('Must multiply 3 and 5 to 15',()=>{
   expect(multiply(3,5)).toBe(15)
+})
+
+test('Must divide 4 by 2 to 2', ()=>{
+  expect(divide(4,2)).toBe(2)
 })
