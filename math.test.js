92c35865b211516d09c98bbfe34da1dcfe10da2f
Digambaran <arjunsreekumar.1@gmail.com>
2023-06-05 13:55:35 +0530

    feat!: new feature prime

--- math.test.js
+++ math.test.js
@@ -1,21 +1,26 @@
-const {add, subtract, multiply, divide, sqrt} = require('./math.js')
+const { add, subtract, multiply, divide, sqrt, primesTill } = require("./math.js");
 
-test('Must add 2 and 3 to 5',()=>{
-  expect(add(2,3)).toBe(5)
-})
+test("Must add 2 and 3 to 5", () => {
+  expect(add(2, 3)).toBe(5);
+});
 
-test('Must subtract 3 from 5 to 2',()=>{
-  expect(subtract(5,3)).toBe(2)
-})
+test("Must subtract 3 from 5 to 2", () => {
+  expect(subtract(5, 3)).toBe(2);
+});
 
-test('Must multiply 3 and 5 to 15',()=>{
-  expect(multiply(3,5)).toBe(15)
-})
+test("Must multiply 3 and 5 to 15", () => {
+  expect(multiply(3, 5)).toBe(15);
+});
 
-test('Must divide 4 by 2 to 2', ()=>{
-  expect(divide(4,2)).toBe(2)
-})
+test("Must divide 4 by 2 to 2", () => {
+  expect(divide(4, 2)).toBe(2);
+});
 
-test('Must return 2',()=>{
-  expect(sqrt(4)).toBe(2)
-})
+test("Must return 2", () => {
+  expect(sqrt(4)).toBe(2);
+});
+
+test("Must return [2,3,5]", () => {
+  expect(primesTill(6)[0]).toBe(2);
+  expect(primesTill(6)[2]).toBe(5);
+});
